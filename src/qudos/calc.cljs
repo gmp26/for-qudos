527da268a7a4d749e758c9dd950bdaa5fe83602a
gmp26 <gmp26@cam.ac.uk>
2016-04-05 16:20:39 +0100

    faked H1 and H2 distributions with normals

--- src/qudos/calc.cljs
+++ src/qudos/calc.cljs
@@ -20,10 +20,30 @@
                0.0348422 0.0463867 0.0640427 0.1017762 0.6340773])
 
 (def centiles (partition 2 1 (cons 0 ub-risks)))
-;; ((0 0.0005456) (0.0005456 0.0009988) ... )
+;; ((0 0.0005456) (0.0005456 0.0009988) ... (0.1017762 0.6340773)
 
 (defn uniform-sample-on "uniform sample on the interval [lb, ub]"
   [rng [lb ub]] (+ lb (* (rand01 rng) (- ub lb))))
+
+(defn- normal
+  "Transform a sequence urs of uniform random number in the interval [0, 1)
+   into a sequence of normal random numbers with mean mu and standard
+   deviation sigma."
+  [rng mu sigma]
+  ; This function implements the Kinderman-Monahan ratio method:
+  ;  A.J. Kinderman & J.F. Monahan
+  ;  Computer Generation of Random Variables Using the Ratio of Uniform Deviates
+  ;  ACM Transactions on Mathematical Software 3(3) 257-260, 1977
+  (loop []
+    (let [u1 (rand01 rng)
+          u2* (rand01 rng)
+          u2 (- 1. u2*)
+          s (* 4 (/ (Math.exp (- 0.5)) (Math.sqrt 2.)))
+          z (* s (/ (- u1 0.5) u2))
+          zz (+ (* 0.25 z z) (Math.log u2))]
+      (if (> zz 0)
+        (recur)
+        (+ mu (* sigma z))))))
 
 (defn to-survival-% "convert p(death) to % chance of survival "
   [risk] (* 100 (- 1 risk)))
@@ -33,6 +53,7 @@
   [rng coll]
   (nth coll (random/next-int! rng (count coll))))
 
+
 (defn sampled "take n full precision numeric samples"
   [rng n]
   (map #(to-survival-%
@@ -41,19 +62,25 @@
 
 (defn risk-category "annotate a risk value with category and icon"
   [rng rate] (cond
-           (< rate 90) {:risk :high
-                        :icon (random-choice rng [:child-bed :incubator :toddler-cot :baby-cot])}
-           (<= rate 99) {:risk :medium
-                         :icon (random-choice rng [:toddler :toddler-cot :baby-cot :yboy :ygirl])}
-           (> rate 99) {:risk :low
-                        :icon (random-choice rng [:toddler :yboy :oboy :ygirl :ogirl])}))
+               (< rate 90) {:risk :high
+                            :icon (random-choice rng [:child-bed :incubator :toddler-cot :baby-cot])}
+               (<= rate 99) {:risk :medium
+                             :icon (random-choice rng [:toddler :toddler-cot :baby-cot :yboy :ygirl])}
+               (> rate 99) {:risk :low
+                            :icon (random-choice rng [:toddler :yboy :oboy :ygirl :ogirl])}))
+
+(defn linear
+  "apply a linear transform to a value"
+  [[m c] value]
+  (+ (* m value) c))
 
 (defn decorated "decorate n samples with string representation, icon, and risk band"
-  [rng n] (map #(let [{:keys [risk icon]} (risk-category rng %)]
-                  {:rate      %
-                   :formatted (.toFixed (js/Number. %) 0)
-                   :icon      icon
-                   :risk      risk}) (sampled rng n)))
+  [rng n]
+  (map #(let [{:keys [risk icon]} (risk-category rng %)]
+         {:rate      %
+          :formatted (.toFixed (js/Number. %) 0)
+          :icon      icon
+          :risk      risk}) (sampled rng n)))
 
 (defn simple-frequencies
   "Simpler - and in this case, faster - algorithm for buckets of size 1"
--- src/qudos/main.cljs
+++ src/qudos/main.cljs
@@ -21,28 +21,31 @@
 (defonce seed (atom 0))
 (defonce rng (atom (random/create @seed)))
 
-(defonce simulation (atom {:decorated    (calc/decorated @sample-rng 100) :decorated-death nil
+(defonce simulation (atom {:decorated    (calc/decorated @sample-rng 100)
                            :future-count 0
-                           :future-seed  0}))
-
-#_(defn survival-count "calculate a random mask for a sample of n operations, where true means dead"
-    [rng decorated-sample]
-    (count (filter #(>= (:rate %) (calc/rand-n rng 100)) decorated-sample)))
+                           :future-seed  0
+                           :m            1
+                           :c            0}))
 
 (defn survival-count
   "find the deaths for a given future index"
   [index decorated-sample]
   (let [sim @simulation
         rng (random/create (+ index (:future-seed sim)))]
-    (count (filter #(>= (:rate %) (calc/rand-n rng 100)) decorated-sample))))
-
-
+    (- 100 (Math.round (calc/normal rng (:m sim) (:c sim))))
+    #_(count (filter #(>= (:rate %) (calc/rand-n rng 100)) decorated-sample))))
 
 (defn resample
   [n]
-  (reset! sample-seed n)
-  (reset! sample-rng (random/create @sample-seed))
-  (swap! simulation assoc :decorated (calc/decorated @sample-rng 100)))
+  (let [sim @simulation]
+    (reset! sample-seed n)
+    (reset! sample-rng (random/create @sample-seed))
+    (swap! simulation assoc :decorated (calc/decorated @sample-rng 100))))
+
+(defn rescale
+  [m c]
+  (swap! simulation assoc :m m :c c)
+  )
 
 (defn reseed
   [n]
@@ -50,6 +53,7 @@
   (reset! rng (random/create @seed)))
 
 (resample 0)
+(rescale 1 0)
 (reseed 1)
 
 (defn death-mask
@@ -58,13 +62,13 @@
   (let [sim @simulation
         rng (random/create (dec (+ index (:future-seed sim))))]
     (js/setTimeout #(resample @sample-seed) 200)
-    (map #(if (< (:rate %) (calc/rand-n rng 100)) (assoc % :risk mask) %) decorated-sample)
-    )
-  )
-
+    (let [deaths (Math.round (calc/normal rng (:m sim) (:c sim)))]
+      (map-indexed #(if (< %1 deaths) (assoc %2 :risk mask) %2) decorated-sample))
+    #_(map #(if (< (:rate %) (calc/rand-n rng 100)) (assoc % :risk mask) %) decorated-sample)))
 
 (rum/defc
-  icon-block < rum/reactive []
+  icon-block < rum/reactive  []
+
   [:div
    (for [row (partition 20 (:decorated (rum/react simulation)))]
      [:div {:style {:zoom 0.1}}
@@ -76,17 +80,11 @@
                                       "fill-" "")
                           (name (:icon item)) ".png")}]])])])
 
-
 (defn show-deaths
   "show/hide deaths in the icon array for the current future"
   [mask]
   (let [sim @simulation]
-    (swap! simulation assoc :decorated (death-mask (:future-count sim) (:decorated sim) mask)))
-  )
-
-#_(def data (take 60 (repeatedly #(+ 90 (* 11 (rand))))))
-
-#_(def more-data (take 100 (repeatedly #(+ 90 (* 11 (rand))))))
+    (swap! simulation assoc :decorated (death-mask (:future-count sim) (:decorated sim) mask))))
 
 (defn hist
   [rates1 & [rates2]]
@@ -100,53 +98,74 @@
 
 (rum/defc root < rum/reactive
           [rates]
-          [:.container
-           [:h1 "Icon array"]
-           [:.row
-            [:.col-md-7
-             (icon-block)
-             [:div {:style {:margin-top " 20px"}}]
-             (hist (map-indexed #(survival-count %1 (:decorated (rum/react simulation))) (range (:future-count (rum/react simulation)))))]
-            ;(map #(calc/survival-count @rng (:decorated (rum/react simulation))) (range 500)))
+          (let [sim (rum/react simulation)]
+            [:.container
+             [:h2 "100 operations"]
+             [:.row
+              [:.col-md-7
+               (icon-block)
+               [:div {:style {:margin-top " 20px"}}]
+               (hist (map-indexed #(survival-count %1 (:decorated (rum/react simulation)))
+                                  (range (:future-count (rum/react simulation)))))]
+              ;(map #(calc/survival-count @rng (:decorated (rum/react simulation))) (range 500)))
 
-            [:form.form-horizontal.col-md-5 {:style {:border "1px solid #CCCCCC"}}
-             [:.form-group
-              [:h3.col-md-12 "Controls"]]
-             [:.form-group
-              [:label.col-md-4{:for "sample"} " sample: "]
-              [:input#sample.col-md-8 {:style     {:width "90px"
-                                                   :zoom  1.5}
-                                       :type      "number"
-                                       :value     (rum/react sample-seed)
-                                       :on-change #(resample (.parseInt js/Number. (.. % -target -value)))}]]
+              [:form.form-horizontal.col-md-5 {:style {:border "1px solid #CCCCCC"}}
+               [:.form-group
+                [:h3.col-md-12 "Controls"]]
 
-             #_[:.form-group
-                [:label.col-md-4 {:for "seed"} " seed: "]
-                [:input#seed.col-md-8 {:style     {:width "90px"
-                                                   :zoom  1.5}
-                                       :type      "number"
-                                       :value     (rum/react seed)
-                                       :on-change #(reseed (.parseInt js/Number. (.. % -target -value)))}]]
+               [:.form-group
+                [:label.col-md-3.text-right {:for "sample"} " sample: "]
+                [:input#sample.col-md-5 {:style     {:width "90px"
+                                                     :zoom  1
+                                                     :margin-right "20px"}
+                                         :type      "number"
+                                         :value     (rum/react sample-seed)
+                                         :on-change #(resample (js/parseInt (.. % -target -value)))}]]
 
-             [:.form-group
-              [:label.col-md-4 {:for "fcount"} " futures: "]
-              [:input#fcount.col-md-10 {:style     {:width "90px"
-                                                    :zoom  1.5}
+               [:.form-group
+                [:label.col-md-3.text-right {:for "mu"} [:i " mu"]]
+                [:input#m.col-md-5 {:style     {:width "90px"
+                                                :zoom  1}
+                                    :type      "number"
+                                    :value     (:m sim)
+                                    :on-change #(rescale (js/parseFloat (.. % -target -value)) (:c sim))}]
+
+                [:label.col-md-3.text-right {:for "c"} [:i " sigma"]]
+                [:input#c.col-md-5 {:style     {:width "90px"
+                                                :zoom  1}
+                                    :type      "number"
+                                    :value     (:c sim)
+                                    :on-change #(rescale (:m sim) (js/parseFloat (.. % -target -value)))}]]
+
+               [:hr]
+               [:.form-group
+                [:label.col-md-7.text-right {:for "pr"} " predicted range: "]
+
+                ;; choose 0/mu=4.5/sigma=0.7/futures=60/future-seed=76 yay!
+                [:label#pr.col-md-5 94 "% - " 97 "%"]
+
+                ;; choose 0/mu=3.5/sigma=1.1/futures=60/future-seed=126
+                [:label.col-md-7.text-right {:for "epr"} " extended predicted range: "]
+                [:label#epr.col-md-5 95 "% - " 99 "%"]]
+
+               [:.form-group
+                [:label.col-md-7.text-right {:for "fcount"} " futures: "]
+                [:input#fcount.col-md-5 {:style     {:width "90px"
+                                                     :zoom  1}
+                                         :type      "number"
+                                         :value     (:future-count (rum/react simulation))
+                                         :on-change #(do
+                                                      (swap! simulation assoc
+                                                             :future-count (js/parseInt (.. % -target -value)))
+                                                      (show-deaths :dead))}]]
+               [:.form-group
+                [:label.col-md-7.text-right {:for "fseed"} " future-seed: "]
+                [:input#fseed.col-md-5 {:style     {:width "90px"
+                                                    :zoom  1}
                                         :type      "number"
-                                        :value     (:future-count (rum/react simulation))
-                                        :on-change #(do
-                                                     (swap! simulation assoc :future-count (.parseInt js/Number. (.. % -target -value)))
-                                                     (show-deaths :dead))}]]
-             [:.form-group
-              [:label.col-md-4 {:for "fseed"} " future-seed: "]
-              [:input#fseed.col-md-10 {:style     {:width "90px"
-                                                   :zoom  1.5}
-                                       :type      "number"
-                                       :value     (:future-seed (rum/react simulation))
-                                       :on-change #(swap! simulation assoc :future-seed (.parseInt js/Number. (.. % -target -value)))}]]
-             ]]
-
-           ])
+                                        :value     (:future-seed (rum/react simulation))
+                                        :on-change #(swap! simulation assoc
+                                                           :future-seed (js/parseInt (.. % -target -value)))}]]]]]))
 
 (defn el [id] (js/document.getElementById id))
 
